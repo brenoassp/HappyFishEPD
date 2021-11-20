6059bf262ba4a9067d777c38fa6610dd6b97c9aa
lcsjunior <lcsjuniorsi@gmail.com>
2021-11-20 17:01:23 -0300

    epd

--- ESP32Helper.h
+++ ESP32Helper.h
@@ -1,6 +1,15 @@
 #ifndef _ESP32HELPER_H_
 #define _ESP32HELPER_H_
 
+#include <Arduino.h>
 
+static const uint8_t   DC_PIN =   5;
+static const uint8_t  SCK_PIN =   7;
+static const uint8_t MISO_PIN =   9;
+static const uint8_t MOSI_PIN =  11;
+static const uint8_t   SS_PIN =  12;
+static const uint8_t BUSY_PIN =  16;
+static const uint8_t  SDA_PIN =  33;
+static const uint8_t  SCL_PIN =  35;
 
 #endif
--- HappyFishEPD.ino
+++ HappyFishEPD.ino
@@ -1,5 +1,30 @@
-void setup() {
+#define ENABLE_GxEPD2_GFX 0
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <GxEPD2_7C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Wire.h>
+#include "Config.h"
+#include "ESP32Helper.h"
+#include "PersWiFiManager.h"
 
+GxEPD2_BW<GxEPD2_290, GxEPD2_290::HEIGHT> display(GxEPD2_290(/*CS=*/ SS_PIN, /*DC=*/ DC_PIN, /*RST=*/ MISO_PIN, /*BUSY=*/ BUSY_PIN));
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  display.init();
+  // comment out next line to have no or minimal Adafruit_GFX code
+  display.setTextColor(GxEPD_BLACK);
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    // comment out next line to have no or minimal Adafruit_GFX code
+    display.print("Hello World!");
+  }
+  while (display.nextPage());
 }
 
 void loop() {
