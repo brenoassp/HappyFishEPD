12d3bd352d93f3d614a7e2b1d034bd9944c931ef
lc <lcsjuniorsi@gmail.com>
2021-12-12 19:44:20 -0300

    config

---
+++ Config.cpp
@@ -0,0 +1,88 @@
+#include "Config.h"
+
+const char *configFilePath = "/config.txt";
+
+void ServerConfig::load(JsonObjectConst obj) {
+  strncpy_P(hostname,
+            obj["hostname"] | "myesp32",
+            sizeof(hostname));
+  strncpy_P(user,
+            obj["user"] | "admin",
+            sizeof(user));
+  strncpy_P(pass,
+            obj["pass"] | "admin",
+            sizeof(pass));
+}
+
+void ServerConfig::save(JsonObject obj) const {
+  obj["hostname"] = hostname;
+  obj["user"] = user;
+  obj["pass"] = pass;
+}
+
+void AlarmsConfig::load(JsonObjectConst obj) {
+  on = obj["on"];
+  off = obj["off"];
+}
+
+void AlarmsConfig::save(JsonObject obj) const {
+  obj["on"] = on;
+  obj["off"] = off;
+}
+
+void Config::load(JsonObjectConst obj) {
+  server.load(obj["server"]);
+  alarms.load(obj["alarms"]);
+}
+
+void Config::save(JsonObject obj) const {
+  server.save(obj.createNestedObject("server"));
+  alarms.save(obj.createNestedObject("alarms"));
+}
+
+bool deserializeConfig(Stream &src, Config &config) {
+  const size_t capacity = 1024;
+  StaticJsonDocument<capacity> doc;
+  DeserializationError error = deserializeJson(doc, src);
+  if (error) return false;
+  config.load(doc.as<JsonObject>());
+  return true;
+}
+
+bool serializeConfig(const Config &config, Print &dest) {
+  const size_t capacity = 1024;
+  StaticJsonDocument<capacity> doc;
+  JsonObject root = doc.to<JsonObject>();
+  config.save(root);
+  return serializeJson(doc, dest) > 0;
+}
+
+bool loadConfigFile(const char *filename, Config &config) {
+  File file = LittleFS.open(filename, "r");
+  if (!file) {
+    Serial.println(F("Failed to open config file"));
+    return false;
+  }
+  bool success = deserializeConfig(file, config);
+  file.close();
+  if (!success) {
+    Serial.println(F("Failed to deserialize config file"));
+    return false;
+  }
+  return true;
+}
+
+bool saveConfigFile(const char *filename, const Config &config) {
+  File file = LittleFS.open(filename, "w");
+  if (!file) {
+    Serial.println(F("Failed to create config file"));
+    return false;
+  }
+  bool success = serializeConfig(config, file);
+  file.close();
+  if (!success) {
+    Serial.println(F("Failed to serialize config file"));
+    return false;
+  }
+  return true;
+}
---
+++ Config.h
@@ -0,0 +1,37 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <LittleFS.h>
+
+extern const char *configFilePath;
+
+struct ServerConfig {
+  char hostname[32];
+  char user[32];
+  char pass[32];
+  void load(JsonObjectConst);
+  void save(JsonObject) const;
+};
+
+struct AlarmsConfig {
+  int32_t on;
+  int32_t off;
+  void load(JsonObjectConst);
+  void save(JsonObject) const;
+};
+
+struct Config {
+  ServerConfig server;
+  AlarmsConfig alarms;
+  void load(JsonObjectConst);
+  void save(JsonObject) const;
+};
+
+bool deserializeConfig(Stream &src, Config &config);
+bool serializeConfig(const Config &config, Print &dest);
+bool loadConfigFile(const char *filename, Config &config);
+bool saveConfigFile(const char *filename, const Config &config);
+
+#endif
